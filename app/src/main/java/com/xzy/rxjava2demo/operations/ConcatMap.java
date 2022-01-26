79c661fd599563a0ae0e9e10437464c727954199
hgncxzy <xuzhuyun@fcbox.com>
2022-01-26 15:54:51 +0800

    feature:添加concatMap用例.

--- app/src/main/java/com/xzy/rxjava2demo/operations/ConcatMap.java
+++ app/src/main/java/com/xzy/rxjava2demo/operations/ConcatMap.java
@@ -3,15 +3,21 @@ package com.xzy.rxjava2demo.operations;
 import android.annotation.SuppressLint;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.xzy.rxjava2demo.MyLogger;
+
+import org.reactivestreams.Publisher;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 
 /**
@@ -45,5 +51,40 @@ public class ConcatMap {
             Log.i(TAG, "accept: " + s);
             MyLogger.INSTANCE.d(TAG, "accept: " + s);
         });
+
+        String[] arr = {"1", "2", "3"};
+        Flowable.fromArray(arr)
+                .concatMap(new Function<String, Publisher<String>>() {
+                    @Override
+                    public Publisher<String> apply(@NonNull String s) throws Exception {
+                        return Flowable.just(s);
+                    }
+                })
+                .map(new Function<String, String>() {
+                    @Override
+                    public String apply(@NonNull String s) throws Exception {
+                        return s + 100;
+                    }
+                })
+                .flatMap(new Function<String, Publisher<?>>() {
+                    @Override
+                    public Publisher<?> apply(@NonNull String s) throws Exception {
+                        return Flowable.just(s);
+                    }
+                })
+                .doOnNext(new Consumer() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.d("xzy", "xzy--" + o.toString());
+                    }
+                })
+                .subscribe();
+
+        /**
+         *
+         * 2022-01-26 15:49:58.715 27039-27039/com.xzy.rxjava2demo D/xzy: xzy--1100
+         * 2022-01-26 15:49:58.715 27039-27039/com.xzy.rxjava2demo D/xzy: xzy--2100
+         * 2022-01-26 15:49:58.716 27039-27039/com.xzy.rxjava2demo D/xzy: xzy--3100
+         * **/
     }
 }
